6ab5370e7bbd27f106f2bfe18d9d5315d25974c8
Andrzej1991 <andrzej.perski@hotmail.com>
2016-05-23 19:54:43 +0200

    config desktop luncher

--- core/src/com/andrzej/company/TutorialClickerClass.java
+++ core/src/com/andrzej/company/TutorialClickerClass.java
@@ -1,27 +1,35 @@
 package com.andrzej.company;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.andrzej.company.screens.SplashScreen;
+import com.badlogic.gdx.Game;
 
-public class TutorialClickerClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class TutorialClickerClass extends Game {
+	public final static String GAME_NAME = "Clicker";
+	public final static  int WIDTH = 480;
+	public final static int HEIGHT = 700;
+
+
+
+	private boolean paused;
+
+
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new SplashScreen(this));
+
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+
+	//getters and setters
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 }
---
+++ core/src/com/andrzej/company/screens/AbstractScreen.java
@@ -0,0 +1,73 @@
+package com.andrzej.company.screens;
+
+import com.andrzej.company.TutorialClickerClass;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+/**
+ * Created by Andrzej on 2016-05-23.
+ */
+public abstract class AbstractScreen implements Screen {
+    protected TutorialClickerClass game;
+    protected Stage stage;
+    private OrthographicCamera camera;
+    protected SpriteBatch spriteBatch;
+
+    public AbstractScreen(TutorialClickerClass game) {
+        this.game = game;
+        createCamera();
+        stage = new Stage(new StretchViewport(TutorialClickerClass.WIDTH, TutorialClickerClass.HEIGHT, camera));
+        spriteBatch = new SpriteBatch();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    private void createCamera() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, TutorialClickerClass.WIDTH, TutorialClickerClass.HEIGHT);
+        camera.update();
+    }
+
+    @Override
+    public void render(float delta) {
+        clearScreen();
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void resume() {
+        game.setPaused(false);
+    }
+
+    @Override
+    public void pause() {
+        game.setPaused(true);
+    }
+
+    @Override
+    public void dispose() {
+        game.dispose();
+    }
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void hide() {}
+
+}
