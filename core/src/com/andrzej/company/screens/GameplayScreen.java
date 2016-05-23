057f9485dd1731d53741569455770cda312b54bd
Andrzej1991 <andrzej.perski@hotmail.com>
2016-05-23 21:26:11 +0200

    Displayed player image on scene

--- core/src/com/andrzej/company/screens/GameplayScreen.java
+++ core/src/com/andrzej/company/screens/GameplayScreen.java
@@ -1,17 +1,39 @@
 package com.andrzej.company.screens;
 
 import com.andrzej.company.TutorialClickerClass;
+import com.andrzej.company.entities.Player;
 
 /**
  * Created by Andrzej on 2016-05-23.
  */
 public class GameplayScreen extends AbstractScreen {
+    private Player player;
+
     public GameplayScreen(TutorialClickerClass game) {
         super(game);
         init();
     }
 
     private void init() {
+        initPlayer();
 
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
     }
 }
