89f6705083db7f45d05975f434d13bff7277387e
Andrzej1991 <andrzej.perski@hotmail.com>
2016-05-23 21:08:34 +0200

    GamePlayScreen class

---
+++ core/src/com/andrzej/company/screens/GameplayScreen.java
@@ -0,0 +1,17 @@
+package com.andrzej.company.screens;
+
+import com.andrzej.company.TutorialClickerClass;
+
+/**
+ * Created by Andrzej on 2016-05-23.
+ */
+public class GameplayScreen extends AbstractScreen {
+    public GameplayScreen(TutorialClickerClass game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
