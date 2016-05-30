a45e620afc741cf7bc8774ae63ccee32ef5b208b
Andrzej1991 <andrzej.perski@hotmail.com>
2016-05-30 21:05:48 +0200

    saving player score

--- core/src/com/andrzej/company/TutorialClickerClass.java
+++ core/src/com/andrzej/company/TutorialClickerClass.java
@@ -2,40 +2,47 @@ package com.andrzej.company;
 
 import com.andrzej.company.screens.SplashScreen;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class TutorialClickerClass extends Game {
+	public final static String GAME_PREFS = "com.andrzej.company.prefs";
+	public final static String GAME_SCORE = "com.andrzej.company.score";
 	public final static String GAME_NAME = "Clicker";
 	public final static  int WIDTH = 480;
 	public final static int HEIGHT = 700;
+	private com.badlogic.gdx.Preferences preferences;
+	private int points;
+	private boolean paused;
 
 	public int getPoints() {
 		return points;
 	}
 	public void addPoint(){
 		points++;
+		preferences.putInteger(GAME_SCORE, points);
+		preferences.flush();
 	}
-
-	private int points;
-
-	private boolean paused;
-
-
-
-	
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
 
 	}
 
+	private void init() {
+		preferences = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+		
+	}
 
+	private void loadScore() {
+		points = preferences.getInteger(GAME_SCORE, points);
+	}
 
 	//getters and setters
-
 	public boolean isPaused() {
 		return paused;
 	}
-
 	public void setPaused(boolean paused) {
 		this.paused = paused;
 	}
